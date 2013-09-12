873e5a7273bcadab2e984479e2fde0e9ea4467b3
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-12 15:49:10 +0800

    Written the root view controller.

--- View Switcher/View Switcher/BIDSwitchViewController.m
+++ View Switcher/View Switcher/BIDSwitchViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "BIDSwitchViewController.h"
+#import "BIDYellowViewController.h"
+#import "BIDBlueViewController.h"
 
 @implementation BIDSwitchViewController
+@synthesize yellowViewController;
+@synthesize blueViewController;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -25,6 +29,12 @@
     [super didReceiveMemoryWarning];
     
     // Release any cached data, images, etc that aren't in use.
+    if(self.blueViewController.view.superview ==nil)
+    {
+        self.blueViewController=nil;
+    }else{
+        self.yellowViewController =nil;
+    }
 }
 
 #pragma mark - View lifecycle
@@ -38,11 +48,13 @@
 
 /*
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+*/
 - (void)viewDidLoad
 {
+    self.blueViewController = [[BIDBlueViewController alloc] initWithNibName:@"BlueView" bundle:nil];
+    [self.view insertSubview:self.blueViewController.view atIndex:0];
     [super viewDidLoad];
 }
-*/
 
 - (void)viewDidUnload
 {
@@ -55,6 +67,23 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+-(IBAction)switchViews:(id)sender{
+    if (self.yellowViewController.view.superview ==nil)
+    {
+        if (self.yellowViewController==nil){
+        self.yellowViewController = [[BIDYellowViewController alloc] initWithNibName:@"YellowView" bundle:nil];
+        }
+        [blueViewController.view removeFromSuperview];
+        [self.view insertSubview:self.yellowViewController.view atIndex:0];
+    }else{
+        if (self.blueViewController==nil){
+            self.blueViewController = [[BIDBlueViewController alloc] initWithNibName:@"BlueView" bundle:nil];
+        }
+        [yellowViewController.view removeFromSuperview];
+        [self.view insertSubview:self.yellowViewController.view atIndex:0];
+        
+    }
 }
 
 @end
