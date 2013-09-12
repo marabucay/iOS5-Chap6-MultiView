b82ab5eed72d9ac79e6b0188b9a786e5e6eb7641
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-12 14:56:49 +0800

    Created SwitchController, BlueViewController and YellowViewController.

---
+++ View Switcher/View Switcher/BIDBlueViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDBlueViewController.h
+//  View Switcher
+//
+//  Created by Maria Alice C. Lim on 9/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDBlueViewController : UIViewController
+
+@end
---
+++ View Switcher/View Switcher/BIDYellowViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDYellowViewController.h
+//  View Switcher
+//
+//  Created by Maria Alice C. Lim on 9/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDYellowViewController : UIViewController
+
+@end
